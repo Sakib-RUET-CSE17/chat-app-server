a5a6a0456bfda3e1d066402a30826133c71ce744
Sakib Hasan <hsakib8685@gmail.com>
2021-06-29 00:44:49 +0600

    client origin rechanged

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express()
 const server = http.createServer(app)
 const io = socketio(server, {
     cors: {
-        origin: 'https://chat-app-c101d.web.app/'
+        origin: 'https://chat-app-c101d.web.app'
     }
 })
 
